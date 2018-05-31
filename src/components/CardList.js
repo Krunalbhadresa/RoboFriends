868e91f9f922cdffb15e00cdd7ec390652703fb9
Krunal Bhadresha <krunalbhadresa@gmail.com>
2018-05-31 19:52:57 -0400

    Scrollable robots and external json data

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from './Card';
-// import { robots } from './robots.js';
+
 
 const CardList = ({ robots }) => {
 	const cardArray = robots.map((user, i) => {
